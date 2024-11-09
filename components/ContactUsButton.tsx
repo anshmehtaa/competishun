74bd253dde81f2381e02a3d30b122da44ec1f31e
yash24shukla <thisform240@gmail.com>
2024-11-09 20:23:18 +0530

    website with responsiveness - 70

--- components/ContactUsButton.tsx
+++ components/ContactUsButton.tsx
@@ -78,7 +78,7 @@ const ContactUsButton = ({ variant = "primary", openDialog = false }) => {
         </button>
       </DialogTrigger>
       <DialogContent
-        className="sm:max-w-md rounded-3xl p-8 bg-primary"
+        className="sm:max-w-md rounded-3xl p-4 sm:p-8 bg-primary scale-75 sm:scale-100"
         style={{ borderRadius: 20 }}
       >
         <DialogHeader>
