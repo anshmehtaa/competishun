1ce65a5e73901152491d1ef3432245570421a166
yash24shukla <thisform240@gmail.com>
2024-11-12 15:15:46 +0530

    website with responsiveness - 73

--- app/page.tsx
+++ app/page.tsx
@@ -111,7 +111,6 @@ export default function Home() {
     <div className="w-full max-w-screen flex relative flex-col items-center">
       <div id="chat-widget" data-key="670f87c42f6b943716677af3"></div>
       <LeadFormWidget />
-      <FormPopup openDialog={true} />
       <div className={"w-11/12 flex flex-col"}>
         <section
           className={"hero-section flex flex-col w-full h-[120vh]  relative"}
